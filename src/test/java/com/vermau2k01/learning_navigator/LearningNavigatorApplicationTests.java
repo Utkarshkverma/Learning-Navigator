7f0438f13a29d2e3dd26095eab2609673eb1c1ed
Utkarsh <vermau2k01@gmail.com>
2024-07-08 20:11:11 +0530

    first commit

---
+++ src/test/java/com/vermau2k01/learning_navigator/LearningNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.learning_navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
